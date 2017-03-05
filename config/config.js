30b565bd142beb581323d1f2b9f2fce869ceb0b3
Sapna Sivakumar <sivakumarsapna@gmail.com>
2017-03-05 22:47:29 +0100

    mysql configurations changed

--- config/config.js
+++ config/config.js
@@ -4,25 +4,11 @@ module.exports = {
       "username": "root",
       "password": "test",
       "database": "database_development",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
-    },
-    "test": {
-      "username": "root",
-      "password": "test",
-      "database": "database_test",
-      "host": "127.0.0.1",
+      "host": "localhost",
       "dialect": "mysql"
     },
     "staging": {
         "url": process.env.DATABASE_URL,
         "dialect": "postgres"
-    },
-    "production": {
-      "username": "root",
-      "password": "test",
-      "database": "database_production",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
     }
 };
